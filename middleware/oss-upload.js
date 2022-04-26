f1786abafd7677b3c0d7e059bf30f1dff85c433d
Flandre3569 <1332726596@qq.com>
2022-04-26 23:41:05 +0800

    [update] ali-oss config

--- middleware/oss-upload.js
+++ middleware/oss-upload.js
@@ -1,22 +1,40 @@
-const config = require('../app/config');
+let OSS = require('ali-oss');
+const { REGION, ACCESSKEY_ID, ACCESSKEY_SECRET, BUCKET } = require('../app/config');
 
-let OSS = require('ali-oss')
 let client = new OSS({ // 连接OSS实例
-  region: config.REGION,
-  accessKeyId: config.ACCESSKEY_ID,
-  accessKeySecret: config.ACCESSKEY_SECRET,
-  bucket: config.BUCKET
+  region: REGION,
+  accessKeyId: ACCESSKEY_ID,
+  accessKeySecret: ACCESSKEY_SECRET,
+  bucket: BUCKET
 });
+// const rules = [{
+//   // 指定允许跨域请求的来源，支持通配符星号（*），表示允许所有的来源域。
+//   allowedOrigin: '*',
+//   // 指定允许的跨域请求方法，支持GET、PUT、DELETE、POST和HEAD方法。
+//   allowedMethod: 'GET',
+//   // 指定允许跨域请求的响应头。建议无特殊情况下将此项设置为通配符星号（*）。
+//   allowedHeader: '*',
+// }];
+// // 最多允许设置10条跨域资源共享规则。如果配置了相同的规则，则已存在的规则将被覆盖。
+// const putResult = await client.putBucketCORS(config.BUCKET, rules);
 
-const rules = [{
-  // 指定允许跨域请求的来源，支持通配符星号（*），表示允许所有的来源域。
-  allowedOrigin: '*',
-  // 指定允许的跨域请求方法，支持GET、PUT、DELETE、POST和HEAD方法。
-  allowedMethod: 'GET',
-  // 指定允许跨域请求的响应头。建议无特殊情况下将此项设置为通配符星号（*）。
-  allowedHeader: '*',
-}];
-// 最多允许设置10条跨域资源共享规则。如果配置了相同的规则，则已存在的规则将被覆盖。
-const putResult = await client.putBucketCORS(config.BUCKET, rules);
+const uploadAvatar = async () => {
+  try {
+    const filename = `${Date.now()}${req.body.name}`
+    const catalog = `/images/${filename}`
+    /* 此处的catalog指的是上传的文件存储在当前Bucket或Bucket下的指定目录 */
+    let result = await client.put(catalog, Buffer.from(req.body.file, 'base64'));
+    res.send({
+      result
+    })
+  } catch (e) {
+    res.send({
+      result: false,
+      errMsg: e
+    })
+  }
+}
 
-console.log(putResult.res);
+module.exports = {
+  uploadAvatar
+}
--- router/user.js
+++ router/user.js
@@ -3,16 +3,18 @@ const { register, login, success, setProfile, queryProfile } = require('../contr
 const { authSign } = require('../controller/authorization');
 const { userVerify, authVerify } = require('../middleware/verify');
 const { md5Crypto } = require('../middleware/md5-crypto');
+const { uploadAvatar } = require('../middleware/oss-upload');
 
 const userRouter = new Router({
   prefix: '/user'
 })
 
 
-userRouter.post('/register', userVerify, md5Crypto, register);
-userRouter.post('/login', login, authSign);
-userRouter.post('/profile', authVerify, setProfile);
-userRouter.post('/queryProfile', authVerify, queryProfile);
+userRouter.post('/register', userVerify, md5Crypto, register); // 注册
+userRouter.post('/login', login, authSign); // 登录，颁布令牌
+userRouter.post('/profile', authVerify, setProfile); // 初始化个人信息
+userRouter.post('/queryProfile', authVerify, queryProfile); // 查询个人信息
+userRouter.post('/avatarUpload', authVerify, uploadAvatar); // 上传头像到oss
 
 
 userRouter.get('/test', authVerify, success); // 测试用例
