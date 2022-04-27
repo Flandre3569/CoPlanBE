5c938799019eea204cf9174d4ef8394efaa7724b
Flandre3569 <1332726596@qq.com>
2022-04-27 23:37:46 +0800

    [update] requir some little bugs

--- middleware/oss-upload.js
+++ middleware/oss-upload.js
@@ -1,12 +1,13 @@
 let OSS = require('ali-oss');
 const { REGION, ACCESSKEY_ID, ACCESSKEY_SECRET, BUCKET } = require('../app/config');
 
-let client = new OSS({ // 连接OSS实例
+const client = new OSS({ // 连接OSS实例
   region: REGION,
   accessKeyId: ACCESSKEY_ID,
   accessKeySecret: ACCESSKEY_SECRET,
   bucket: BUCKET
 });
+
 // const rules = [{
 //   // 指定允许跨域请求的来源，支持通配符星号（*），表示允许所有的来源域。
 //   allowedOrigin: '*',
@@ -18,21 +19,17 @@ let client = new OSS({ // 连接OSS实例
 // // 最多允许设置10条跨域资源共享规则。如果配置了相同的规则，则已存在的规则将被覆盖。
 // const putResult = await client.putBucketCORS(config.BUCKET, rules);
 
-const uploadAvatar = async () => {
-  try {
-    const filename = `${Date.now()}${req.body.name}`
-    const catalog = `/images/${filename}`
-    /* 此处的catalog指的是上传的文件存储在当前Bucket或Bucket下的指定目录 */
-    let result = await client.put(catalog, Buffer.from(req.body.file, 'base64'));
-    res.send({
-      result
-    })
-  } catch (e) {
-    res.send({
-      result: false,
-      errMsg: e
-    })
-  }
+const uploadAvatar = async (ctx, next) => {
+  console.log(ctx.request);
+  const filename = `${Date.now()}${ctx.request.body}`;
+
+  const catalog = `/images/${filename}`
+  /* 此处的catalog指的是上传的文件存储在当前Bucket或Bucket下的指定目录 */
+  let result = await client.put(catalog, Buffer.from(req.body.file, 'base64'));
+  res.send({
+    result
+  })
+
 }
 
 module.exports = {
--- router/user.js
+++ router/user.js
@@ -1,10 +1,13 @@
 const Router = require('@koa/router');
+const multer = require('@koa/multer')
 const { register, login, success, setProfile, queryProfile } = require('../controller/user');
 const { authSign } = require('../controller/authorization');
 const { userVerify, authVerify } = require('../middleware/verify');
 const { md5Crypto } = require('../middleware/md5-crypto');
 const { uploadAvatar } = require('../middleware/oss-upload');
 
+
+const upload = multer();
 const userRouter = new Router({
   prefix: '/user'
 })
@@ -14,7 +17,7 @@ userRouter.post('/register', userVerify, md5Crypto, register); // 注册
 userRouter.post('/login', login, authSign); // 登录，颁布令牌
 userRouter.post('/profile', authVerify, setProfile); // 初始化个人信息
 userRouter.post('/queryProfile', authVerify, queryProfile); // 查询个人信息
-userRouter.post('/avatarUpload', authVerify, uploadAvatar); // 上传头像到oss
+userRouter.post('/avatarUpload', upload.any(), uploadAvatar); // 上传头像到oss
 
 
 userRouter.get('/test', authVerify, success); // 测试用例
